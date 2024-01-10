e20ad49d6d7005bd8639a7b8b65792ae3189cdfe
sooma93 <tasneemabasit5@gmail.com>
2024-01-10 05:51:16 -0800

    tasneem

---
+++ base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+the base model
+"""
+import uuid
+from datetime import datetime
+from models import storage
+
+class BaseModel:
+    """
+    the class which all class inhernt
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        init attributes
+        """
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
